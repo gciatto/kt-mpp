1235f8966bc9749e37553998b4e49dcadb42e485
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-07-05 10:59:21 +0200

    feat: auto-configure :detektAll a dependency of :check

--- src/main/kotlin/io/github/gciatto/kt/mpp/BugFinderPlugin.kt
+++ src/main/kotlin/io/github/gciatto/kt/mpp/BugFinderPlugin.kt
@@ -1,5 +1,6 @@
 package io.github.gciatto.kt.mpp
 
+import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Project
@@ -9,10 +10,15 @@ class BugFinderPlugin : AbstractProjectPlugin() {
         val detekt = apply(DetektPlugin::class)
         log("apply ${detekt::class.java.name} as bug finder")
         configure(DetektExtension::class) {
-            // toolVersion = "1.19.0"
             config.setFrom(rootProject.files(".detekt.yml"))
             log("configure bug finder from files: ${config.files.joinToString()}")
             buildUponDefaultConfig = true
         }
+        val detektAll = tasks.maybeCreate("detektAll").also { it.group = "verification" }
+        tasks.withType(Detekt::class.java)
+            .matching { task -> task.name.let { it.endsWith("Main") || it.endsWith("Test") } }
+            .all { detektAll.dependsOn(it) }
+        tasks.getByName("check").dependsOn(detektAll)
+        log("add task ${detektAll.path} and make $path:check depend on it")
     }
 }
