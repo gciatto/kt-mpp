b18fdc148d4556d6c5d289e56487c09aabfe8d54
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-09-05 12:05:30 +0200

    chore(build): publish tagged scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,36 @@
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     id("com.gradle.enterprise") version "3.14.1"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.9"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
 }
 
+fun tag(format: String, vararg properties: String): String =
+    format.format(*properties.map { System.getProperty(it) ?: error("Missing property $it") }.toTypedArray())
+
+val ci = !System.getenv("CI").isNullOrBlank()
+val ciTag = if (ci) "CI" else "Local"
+val osTag = "OS: " + tag("%s (%s) v. %s", "os.name", "os.arch", "os.version")
+val jvmTag = "JVM: " + tag("%s v. %s", "java.vm.name", "java.vm.version")
+val whenTag = "When: ${ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT)}"
+
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
+        tag(ciTag)
+        tag(osTag)
+        tag(jvmTag)
+        tag(whenTag)
         publishOnFailure()
+        buildScanPublished {
+            if (ci) {
+                println("::error title=Gradle scan for $osTag, $jvmTag, $whenTag::$buildScanUri")
+            }
+        }
     }
 }
 
