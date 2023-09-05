27d95710f363080c54ae30ababb555e998585cea
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-09-05 10:18:35 +0200

    chore: fix deprecation in AbstractKotlinProjectPlugin

--- src/main/kotlin/io/github/gciatto/kt/mpp/AbstractKotlinProjectPlugin.kt
+++ src/main/kotlin/io/github/gciatto/kt/mpp/AbstractKotlinProjectPlugin.kt
@@ -1,5 +1,6 @@
 package io.github.gciatto.kt.mpp
 
+import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.VersionCatalogsExtension
@@ -237,13 +238,13 @@ abstract class AbstractKotlinProjectPlugin(targetName: String) : AbstractProject
     protected fun configureNodeJs() {
         nodejs {
             log("configure kotlin JS to target NodeJS")
-            testTask {
-                useMocha {
+            testTask(Action {
+                it.useMocha {
                     log("use Mocha as JS test framework")
                     timeout = getProperty("mochaTimeout")
                     log("set Mocha per-test-case timeout to $timeout")
                 }
-            }
+            })
         }
     }
 }
