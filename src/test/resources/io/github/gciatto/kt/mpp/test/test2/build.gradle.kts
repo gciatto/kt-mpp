a62e4e63ad6f208366088c6c71cfddf59c119665
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-09-07 11:21:56 +0200

    test: add test2 for root project adding plugins directly

---
+++ src/test/resources/io/github/gciatto/kt/mpp/test/test2/build.gradle.kts
@@ -0,0 +1,63 @@
+import io.github.gciatto.kt.mpp.ProjectType
+import io.github.gciatto.kt.mpp.log
+import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven
+import org.gradle.api.publish.maven.tasks.PublishToMavenLocal
+import org.gradle.kotlin.dsl.withType
+import org.gradle.api.logging.LogLevel
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("io.github.gciatto.kt-mpp.multiplatform")
+    id("io.github.gciatto.kt-mpp.versions")
+    id("io.github.gciatto.kt-mpp.documentation")
+    id("io.github.gciatto.kt-mpp.maven-publish")
+    id("io.github.gciatto.kt-mpp.npm-publish")
+}
+
+group = "io.github.gciatto"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js {
+        nodejs {
+            binaries.library()
+        }
+    }
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.kotlin.reflect)
+            }
+        }
+    }
+}
+
+val localMavenRepoDir = rootProject.projectDir.absoluteFile.resolve("build/.m2/repository")
+println("Set local Maven repository path: $localMavenRepoDir")
+System.setProperty("maven.repo.local", localMavenRepoDir.absolutePath)
+
+plugins.withId("maven-publish") {
+    extensions.getByType(PublishingExtension::class.java).run {
+        tasks.create("showPublications") {
+            doLast {
+                log("publications: ${publications.joinToString { it.name }}", LogLevel.LIFECYCLE)
+            }
+        }
+    }
+}
+
+tasks.create("printMavenLocal") {
+    doLast {
+        fileTree(localMavenRepoDir).forEach {
+            println(it)
+        }
+    }
+    val printMavenLocal = this
+    tasks.withType<PublishToMavenLocal> {
+        printMavenLocal.mustRunAfter(this)
+    }
+}
