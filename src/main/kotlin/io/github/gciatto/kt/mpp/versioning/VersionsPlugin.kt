97ae17720c255a312d8df376e221b4097d9b1716
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-12-31 14:50:36 +0100

    fix: apply rootProject's version to subprojects in afterEvaluate

--- src/main/kotlin/io/github/gciatto/kt/mpp/versioning/VersionsPlugin.kt
+++ src/main/kotlin/io/github/gciatto/kt/mpp/versioning/VersionsPlugin.kt
@@ -9,10 +9,12 @@ import org.gradle.api.Project
 class VersionsPlugin : AbstractProjectPlugin() {
     override fun Project.applyThisPlugin() {
         val versioning = "versioning"
-        group = rootProject.group
-        log("copy group from rootProject: $group")
-        version = rootProject.version
-        log("copy version from rootProject: $version")
+        project.afterEvaluate {
+            group = rootProject.group
+            log("copy group from rootProject: $group")
+            version = rootProject.version
+            log("copy version from rootProject: $version")
+        }
         tasks.register("printVersion") {
             it.group = versioning
             it.doLast { println(project.version) }
