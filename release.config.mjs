b75b9a300ed696fa5dadb2c7ed712df72a41c957
Giuseppe Pisano <41598901+Gilbocc@users.noreply.github.com>
2025-09-16 15:12:19 +0200

    fix(c1): publish to github

--- release.config.mjs
+++ release.config.mjs
@@ -1,7 +1,7 @@
 const publishCmd = `
 ./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 1
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
-./gradlew publishKotlinOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository || true
+./gradlew publishOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository || true
 `;
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
