b6a166bb890895df33b0fb24ba9c05398d558ac5
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2026-09-05 16:21:14 +0200

    feat: enable logging for projects setting properties showTestsInConsole

--- src/main/kotlin/io/github/gciatto/kt/mpp/helpers/MultiProjectHelperPlugin.kt
+++ src/main/kotlin/io/github/gciatto/kt/mpp/helpers/MultiProjectHelperPlugin.kt
@@ -4,6 +4,8 @@ import io.github.gciatto.kt.mpp.AbstractProjectPlugin
 import io.github.gciatto.kt.mpp.Plugins
 import io.github.gciatto.kt.mpp.utils.log
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
+import org.gradle.kotlin.dsl.withType
 
 class MultiProjectHelperPlugin : AbstractProjectPlugin() {
     override fun Project.applyThisPlugin() {
@@ -14,5 +16,14 @@ class MultiProjectHelperPlugin : AbstractProjectPlugin() {
             extensions.create("multiProjectHelper", MultiProjectExtensionView::class, rootExtension)
         }
         log("apply ${Plugins.multiProjectHelper.name} plugin")
+        if (project.findProperty("showTestsInConsole")?.toString()?.toBoolean() == true) {
+            tasks.withType<Test>().configureEach { task ->
+                task.testLogging {
+                    it.events("passed", "skipped", "failed", "standardOut", "standardError")
+                    it.showStandardStreams = true
+                    log("Enabled logging for ${task.path}")
+                }
+            }
+        }
     }
 }
